84b0448c76d4a604b5df1754cfb4de19964c7a1a
abdulrg01 <abdulrg01@gmail.com>
2025-04-03 10:50:59 +0100

    hero section

--- app/clara/page.tsx
+++ app/clara/page.tsx
@@ -1,15 +1,17 @@
 import AboutSection from "@/components/clara/AboutSection";
 import BiographySection from "@/components/clara/BiographySection";
-import Clara from "@/components/clara/Clara";
+// import Clara from "@/components/clara/Clara";
 import ContactSection from "@/components/clara/ContactSection";
 import Gallary from "@/components/clara/Gallary";
+import HomeHero from "@/components/clara/Hero";
 import InitiativesSection from "@/components/clara/InitiativesSection";
 import React from "react";
 
 export default function page() {
   return (
     <div>
-      <Clara />
+      <HomeHero />
+      {/* <Clara /> */}
       <AboutSection />
       <BiographySection />
       <Gallary />
